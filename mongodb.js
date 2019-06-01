89b97b651cb333139a5fe407f005a699f141a789
Vinceeenttttt <44713187+Vinceeenttttt@users.noreply.github.com>
2019-06-01 13:44:28 +0100

    Update mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -1,12 +1,65 @@
 const mongo = require('mongodb');
 const MongoClient = require('mongodb').MongoClient;
+const moment = require('moment');
 
-var url = "mongodb://localhost:27017/mydb";
+//var url = "mongodb://localhost:27017/mydb";
 
-MongoClient.connect(url, function(err, db) {
+const uri = "mongodb+srv://vincent:vincent@hexology-nkjiq.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//     if (err) throw err;
+//     var dbo = client.db("mydb");
+//     var myobj = { name: "Robert", level: "Beginner", time: moment().format()};
+//     dbo.collection("test").insertOne(myobj, function(err, res) {
+//       if (err) throw err;
+//       console.log("1 document inserted");
+//       client.close();
+//     });
+// });
+
+function insertdoc(CollectionName, Obj){
+  client.connect(err => {
+      if (err) throw err;
+      var dbo = client.db("hexology");
+      dbo.collection(CollectionName).insertOne(Obj, function(err, res) {
+        if (err) throw err;
+        console.log("1 document inserted");
+        client.close();
+      });
+  });
+}
+
+function filterdoc(CollectionName, queryname){
+  client.connect(err => {
     if (err) throw err;
-    var dbo = db.db("mydb");
-    var myobj = { name: "Vincent", level: "Beginner" };
+    var dbo = client.db("hexology");
+    dbo.collection(CollectionName).find(queryname).toArray(function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      client.close();
+    });
+});
+}
+
+var CollectionName = "profiles";
+var myobj = {
+  name: "Robert",
+  level: "Beginner",
+  Interests: "EEE",
+  time: moment().format()
+};
+
+//insertdoc(CollectionName, myobj);
+
+var query = { level: "Beginner"};
+filterdoc(CollectionName, query);
+
+// MongoClient.connect(url, function(err, db) {
+//     if (err) throw err;
+//     var dbo = db.db("mydb");
+//     var myobj = { name: "Bob", level: "Beginner" };
 
     // Create a collection
 
@@ -34,12 +87,12 @@ MongoClient.connect(url, function(err, db) {
 
     // Filter the result
 
-      var query = { level: "expert" };
-      dbo.collection("customers").find(query).toArray(function(err, result) {
-        if (err) throw err;
-        console.log(result);
-        db.close();
-      });
+    //   var query = { level: "Beginner" };
+    //   dbo.collection("customers").find(query).toArray(function(err, result) {
+    //     if (err) throw err;
+    //     console.log(result);
+    //     db.close();
+    //   });
     
     // Delete the document
 
@@ -68,4 +121,4 @@ MongoClient.connect(url, function(err, db) {
     //   db.close();
     // });
 
-});
+// });
