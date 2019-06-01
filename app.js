1cdaf4d1b8c9d9ea3dc707df302012a02f2b9f9b
Vinceeenttttt <44713187+Vinceeenttttt@users.noreply.github.com>
2019-06-01 20:33:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,35 @@ const path = require('path');
 const app = express();
 require('dotenv').config({silent: true});
 const socket = require('socket.io');
+const mongo = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
+const moment = require('moment');
 
+// Create a database
+const uri = "mongodb+srv://vincent:vincent@hexology-nkjiq.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+function insertdoc(CollectionName, Obj){
+  client.connect(err => {
+      if (err) throw err;
+      var dbo = client.db("hexology");
+      dbo.collection(CollectionName).insertOne(Obj, function(err, res) {
+        if (err) throw err;
+        console.log("1 document inserted");
+        client.close();
+      });
+  });
+}
+
+var CollectionName = "profiles";
+var myobj = {
+  name: "Robert",
+  level: "Beginner",
+  Interests: "EEE",
+  time: moment().format()
+};
+
+// Build a server
 var port = process.env.PORT || 5000;
 
 var server = app.listen(port, function(){
@@ -31,7 +59,18 @@ const io = socket(server);
   
       console.log('message: ' + msg);
       io.emit('chat message', "you: " + msg);
-     });
+     
+    });
+
+    socket.on('timer', function(msg) {
+  
+      console.log('message: ' + msg);
+      var start = Date.now();
+      setInterval(function() {
+        insertdoc(CollectionName, myobj)
+      }, 30000);
+     
+    });
   });
   
   module.exports = app;
